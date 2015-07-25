0009fef80b1699852662c78f81e580ab64a8ef3a
zephyrz4 <zephyrz2712@gmail.com>
2015-07-25 11:45:48 -0400

    libgdxproject

---
+++ android/src/com/strayfluxinteractive/learn2fly/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.strayfluxinteractive.learn2fly.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.strayfluxinteractive.learn2fly.Learn2Fly;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Learn2Fly(), config);
+	}
+}
---
+++ desktop/src/com/strayfluxinteractive/learn2fly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.strayfluxinteractive.learn2fly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.strayfluxinteractive.learn2fly.Learn2Fly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Learn2Fly(), config);
+	}
+}
